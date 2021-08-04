6007d776e24576253663df8c7d77e73358f9d2da
Luke Cole <Lukeajcole@gmail.com>
2021-08-03 21:44:10 -0700

    final touches

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 Luke Cole's Portolio Readme
 
-[This is a website](https://lukeajcole.github.io/W2_Portfolio_LAJC/code/index.html) that has been developed by Luke Cole as part of the UCSD Extension Botcamp for full stack web development. This is the second version my portfolio site 
+[This is a website](https://lukeajcole.github.io/W2_Portfolio_LAJC/code/index.html) that has been developed by Luke Cole as part of the UCSD Extension Bootcamp for full stack web development. This is the second version my portfolio site and has been updated with real web apps that I've developed in the class. 
 
 
 Here is a screenshot of the site.
-![image](..\assets\images\site-demo.PNG)
+![image](.\assets\images\site-demo.PNG)
 
-
-The goal for this iteration was to make use of the advanced CSS tools such as media queries, pseudoelements, and flex boxes in order to create a responsive website.
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -9,23 +9,25 @@ $('document').ready(function(){
 		$('#' + background + '-form').removeClass('formgroup-active');
 	});
 
-function errorfield(field){
-	$(field).addClass('formgroup-error');
-	console.log(field);	
-}
+	function errorfield(field){
+		$(field).addClass('formgroup-error');
+		console.log(field);	
+	}
 
-$("#waterform").submit(function() {
-	var stopsubmit = false;
+	$("#waterform").submit(function() {
+		var stopsubmit = false;
 
-if($('#name').val() == "") {
-	errorfield('#name-form');
-	stopsubmit=true;
-}
-if($('#email').val() == "") {
-	errorfield('#email-form');
-	stopsubmit=true;
-}
-  if(stopsubmit) return false;
-});
-		
+		if($('#name').val() == "") {
+			errorfield('#name-form');
+			stopsubmit=true;
+		}
+		if($('#email').val() == "") {
+			errorfield('#email-form');
+			stopsubmit=true;
+		}
+		if(stopsubmit) {return false}
+			else {
+			//Put code to send email here
+			}
+		});
 });
